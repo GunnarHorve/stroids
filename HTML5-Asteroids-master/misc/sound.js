1c6037012f3ded78d4339e3f5ff55f94dd5a8422
GunnarHorve <horvegc@rose-hulman.edu>
2016-04-09 16:04:06 -0400

    Asteroids "game.js" File is broken into more managable chunks.

---
+++ HTML5-Asteroids-master/misc/sound.js
@@ -0,0 +1,31 @@
+SFX = {
+  laser:     new Audio('39459__THE_bizniss__laser.wav'),
+  explosion: new Audio('51467__smcameron__missile_explosion.wav')
+};
+
+// preload audio
+for (var sfx in SFX) {
+  (function () {
+    var audio = SFX[sfx];
+    audio.muted = true;
+    audio.play();
+
+    SFX[sfx] = function () {
+      if (!this.muted) {
+        if (audio.duration == 0) {
+          // somehow dropped out
+          audio.load();
+          audio.play();
+        } else {
+          audio.muted = false;
+          audio.currentTime = 0;
+        }
+      }
+      return audio;
+    }
+  })();
+}
+// pre-mute audio
+SFX.muted = false;
+
+// vim: fdl=0
