79ca4ae60c5b5f4a8739ecc074316f7613e058b4
Matthew <schackma@rose-hulman.edu>
2016-04-10 03:42:02 -0400

    mother fucking bullets

--- asteroids-backend/game/bullet.js
+++ asteroids-backend/game/bullet.js
@@ -1,7 +1,9 @@
 var Sprite = require('./Sprite');
 
 Bullet = function () {
-  this.init("bullet", [0, 0]);
+  this.bullet_length = 1
+  this.init("bullet", [0, 0,
+                       1, 0]);
   this.time = 0;
   this.bridgesH = false;
   this.bridgesV = false;
@@ -11,19 +13,19 @@ Bullet = function () {
   //this.collidesWith = ["asteroid"];
 
   this.configureTransform = function () {};
-  this.draw = function () {
-    if (this.visible) {
-      this.context.save();
-      this.context.lineWidth = 2;
-      this.context.beginPath();
-      this.context.moveTo(this.x-1, this.y-1);
-      this.context.lineTo(this.x+1, this.y+1);
-      this.context.moveTo(this.x+1, this.y-1);
-      this.context.lineTo(this.x-1, this.y+1);
-      this.context.stroke();
-      this.context.restore();
-    }
-  };
+  // this.draw = function () {
+  //   if (this.visible) {
+  //     this.context.save();
+  //     this.context.lineWidth = 2;
+  //     this.context.beginPath();
+  //     this.context.moveTo(this.x-1, this.y-1);
+  //     this.context.lineTo(this.x+1, this.y+1);
+  //     this.context.moveTo(this.x+1, this.y-1);
+  //     this.context.lineTo(this.x-1, this.y+1);
+  //     this.context.stroke();
+  //     this.context.restore();
+  //   }
+  // };
   this.preMove = function (delta) {
     if (this.visible) {
       this.time += delta;
