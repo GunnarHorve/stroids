a649df2fa92daa1eecffdaebf1a41272e58897c6
Zach Dougherty <doughezj@rose-hulman.edu>
2016-04-09 23:34:40 -0400

    Dear Schack:
    you make me sad.
    
    that is all.
    Also, I added in all the work schack removed

--- asteroids-backend/public/javascripts/game/jQuery_bindings.js
+++ asteroids-backend/public/javascripts/game/jQuery_bindings.js
@@ -6,6 +6,8 @@ $(function () {
   Text.context = context;
   Text.face = vector_battle;
 
+  context.font = "15px Arial";
+
   var gridWidth = Math.round(canvas.width() / GRID_SIZE);
   var gridHeight = Math.round(canvas.height() / GRID_SIZE);
   var grid = new Array(gridWidth);
@@ -55,6 +57,18 @@ $(function () {
 
   var canvasNode = canvas[0];
 
+  //message work
+var chatmode = false;
+var currentMessage = "";
+var messages = ["h", "i", "t", "h", "e", "r", "e", "!"];
+var messageTimer=[600, 500, 490, 300, 220, 210, 100, 10];
+//message work end
+
+//leaderboard work top 5
+var leaderNames = ["bob1", "bob2", "bob3", "bob4", "bob5"];
+var leaderScores = [100, 200, 300, 400, 500];
+//leaderboard work end
+
   // shim layer with setTimeout fallback
   // from here:
   // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
@@ -88,27 +102,142 @@ $(function () {
       }
     }
 
+    //leaderboard
+    for(var i = 0; i < leaderNames.length; i++){
+      textY = 20 + 20*(4-i);
+      var boardRow = (5-i) + ": " + leaderNames[i] + " " + leaderScores[i];
+      context.fillText(boardRow, Game.canvasWidth-8*boardRow.length, textY);
+    }
+
+    //chat
+    if(chatmode){
+      context.fillText(currentMessage, 10, Game.canvasHeight - 10);
+      for(i = 0; i < messages.length; i++){
+        textY = Game.canvasHeight - 30 - 15*i;
+        context.fillText(messages[i], 10, textY);
+      }
+    }
+
+    for(var i = 0; i < messages.length; i++){
+      if(messageTimer[i] > 0){
+        textY = Game.canvasHeight - 30 - 15*i;
+        context.fillText(messages[i], 10, textY);
+        messageTimer[i]--;
+      }
+    }
+
+    // display score
+    var score_text = ''+ Game.score;
+    context.fillText(score_text, 15, 20);
+
     requestAnimFrame(mainLoop, canvasNode);
   };
 
   mainLoop();
 
+  function addChatToScreen(string){
+    var tempA = messages[0];
+    var tempA2 = messageTimer[0];
+    var tempB = "";
+    var tempB2 = 0;
+      for(var i = 1; i < messages.length; i++){
+        tempB = messages[i];
+        tempB2 = messageTimer[i];
+        messages[i] = tempA;
+        messageTimer[i] = tempA2;
+        tempA = tempB;
+        tempA2 = tempB2;
+      }
+      messages[0] = string;
+      messageTimer[0] = 1000;
+  }
+
+//checks the given score against the lowest score and adds to list if need be
+  function checkHighScore(name, score){
+    var lowestHigh = leaderScores[0];//scores are 0-4 lowest to highest
+    if (score > lowestHigh){
+      leaderScores[0] = score;
+      leaderNames[0] = name;
+      forceHighScoreSort();
+    }
+  }
+
+  function forceHighScoreSort(){
+    var temp = "";
+    var temp2 = 0;
+    var j = 0;
+    for(var i = 1; i < leaderNames.length; i++){
+      j = i;
+      while(j > 0 && leaderScores[j-1] > leaderScores[j]){
+        swapScore(j,j-1);
+        j--;
+      }
+    }
+  }
+
+  function swapScore(index1, index2){
+    temp1 = leaderNames[index1];
+    temp2 = leaderScores[index1];
+    leaderNames[index1] = leaderNames[index2];
+    leaderScores[index1] = leaderScores[index2];
+    leaderNames[index2] = temp1;
+    leaderScores[index2] = temp2;
+  }
+
+  function resetHighScores(){
+    leaderNames = ["", "", "", "", ""];
+    leaderScores = [0, 0, 0, 0, 0];
+  }
+
   $(window).keydown(function (e) {
     switch (KEY_CODES[e.keyCode]) {
-      case 'f': // show framerate
-        showFramerate = !showFramerate;
-        break;
-      case 'p': // pause
-        paused = !paused;
-        if (!paused) {
-          // start up again
-          lastFrame = Date.now();
-          mainLoop();
+      if(chatmode && KEY_CODES[e.keyCode] != 'enter'){
+          //talk
+          var input = String.fromCharCode(e.keyCode).toLowerCase();
+          if(/[a-zA-Z0-9-_ ]/.test(input)){
+            currentMessage = currentMessage + input;
+          }else if(e.keyCode == 8){//backspace
+            currentMessage = currentMessage.substring(0, currentMessage.length - 1);
+          }else{
+            return;
+          }
+          return;
         }
-        break;
-      case 'm': // mute
-        SFX.muted = !SFX.muted;
-        break;
-    }
+        switch (KEY_CODES[e.keyCode]) {
+          case 'm': // mute
+            SFX.muted = !SFX.muted;
+            break;
+          case 't':
+            if(!chatmode){
+              chatmode = true;
+              ship.chatting = true;
+              //Game.isChatting = true;
+              break;
+            }
+            break;
+          case '/':
+            if(!chatmode){
+              chatmode = true;
+              ship.chatting = true;
+              //Game.isChatting = true;
+              break;
+            }
+          break;
+          case 'enter':
+          if(!chatmode){
+            forceHighScoreSort();
+            break;
+          }
+          chatmode = false;
+          ship.chatting = false;
+          //Game.isChatting = false;
+          if(currentMessage == "" || currentMessage == " " || currentMessage == "  "){
+            currentMessage = "";
+            break;
+          }
+          addChatToScreen(currentMessage);
+          currentMessage = "";
+          break;
+        }
   });
 });
